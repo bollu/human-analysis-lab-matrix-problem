aae92a8c8f0af8be3a2474f32f6eaa4424cf7e33
Siddharth Bhat <siddu.druid@gmail.com>
2018-08-13 01:36:41 +0530

    create a test harness like thing

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY=run
 
 mat.out: main.cpp matrix.h
-	clang main.cpp -o mat.out
+	clang main.cpp -o mat.out -lstdc++ -std=c++14
 
 run: mat.out
 	./mat.out
--- matrix.h
+++ matrix.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <array>
 // D = number of dimensions per diagonal
 // B = number of blocks
@@ -8,12 +9,12 @@ struct DiagMatrix {
 };
 
 template<int D, int B, typename T>
-DiagMatrix<D, B, T> mul(DiagMatrix<D, B, T> m1, DiagMatrix<D, B, T> d2) {
+DiagMatrix<D, B, T> mulDiag(DiagMatrix<D, B, T> m1, DiagMatrix<D, B, T> d2) {
     // we can perform block wise multiplication
 };
 
 template<int D, int B, typename T>
-DiagMatrix<D, B, T> inv(DiagMatrix<D, B, T> m1, DiagMatrix<D, B, T> d2) {};
+DiagMatrix<D, B, T> invDiag(DiagMatrix<D, B, T> m1, DiagMatrix<D, B, T> d2) {};
 
 template<int D, int B, typename T>
 using RawMatrix = std::array< std::array<T, D * B>, D * B>;
@@ -30,6 +31,55 @@ RawMatrix<D, B, T> mkRawMatrix(DiagMatrix<D, B, T> m) {
         }
     }
     return raw;
+}
+
+template<int D, int B, typename T>
+RawMatrix<D, B, T> mulRawMatrix(RawMatrix<D, B, T> m1, RawMatrix<D, B, T>m2) {
+    RawMatrix<D, B, T> raw;
+    for(int i = 0; i < D * B; i++) {
+        for(int j = 0; j < D * B; j++) {
+                raw[i][j] = 0;
+            for(int k = 0; k < D * B; k++) {
+                raw[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    return raw;
+}
+
+enum class LogLevel {
+    LogOn = 1,
+    LogOff = 0
+};
+template<int D, int B, typename T>
+bool isRawEqual(RawMatrix<D, B, T> r1, RawMatrix<D, B, T> r2, LogLevel l) {
+    for(int i = 0; i < D * B; i++) {
+        for(int j = 0; j < D * B; j++) {
+            if (r1[i][j] != r2[i][j]) {
+
+                // we need to log
+                if (l == LogLevel::LogOn) {
+                    std::cout << "matrices differ at: (" << i << "," << j <<")\n";
+                    std::cout << "r1[" << i << "][" << j << "] = " << r1[i][j] << "\n";
+                    std::cout << "r2[" << i << "][" << j << "] = " << r2[i][j] << "\n";
+                }
+                
+
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+template<int D, int B, typename T>
+void checkMatmul(DiagMatrix<D, B, T> d1, DiagMatrix<D, B, T> d2) {
+    DiagMatrix<D, B, T> diag  = mulDiag(d1, d2);
+    RawMatrix<D, B, T> raw = mulRawMatrix(mkRawMatrix(d1), mkRawMatrix(d2));
+    RawMatrix<D, B, T> diag2raw = mkRawMatrix(diag);
+
+    assert(isRawEqual(raw, diag2raw));
 }
 
 
