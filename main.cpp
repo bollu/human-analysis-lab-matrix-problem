fdb96c33ccef64ee3e7aa78c7eece768718793e3
Siddharth Bhat <siddu.druid@gmail.com>
2018-08-13 01:53:03 +0530

    [WIP] this seems to work? I'm surprised
    Straight line code hacked at 2AM worked :)

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include "matrix.h"
 
-static const int NUM_MUL_CHECKS = 1000;
+static const int NUM_MUL_CHECKS = 100;
 static const int D = 10;
 static const int B = 10;
 
 
 int main() {
     for(int i = 0; i < NUM_MUL_CHECKS; i++) {
+        std::cout << "running matmul check (" << i << ")...";
         DiagMatrix<D, B, int> m1 = genRandDiagIntMatrix<D, B>();
         DiagMatrix<D, B, int> m2 = genRandDiagIntMatrix<D, B>();
 
         checkMatmul(m1, m2);
+        std::cout << "success\n";
 
     }
     return 0;
